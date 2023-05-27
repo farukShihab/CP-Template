8bf260ee67afb6c27c109778dbcfb6ad0b7ca30d
Faruk Shihab <120820358+farukShihab@users.noreply.github.com>
2023-05-27 19:48:42 +0600

    Create temlpate.cpp

---
+++ temlpate.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned long long int
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<long long>
+#define di deque<int>
+#define dll deque<long long>
+#define CY cout<< "Yes"
+#define CN cout<< "No"
+#define C(x) cout<<x<<endl
+#define pi acos(-1)
+#define sc second
+#define fi first
+#define testCase int t;cin>>t;while(t--)
+#define faster ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL)
+
+
+int main()
+{
+    faster;
+
+
+    testCase
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
